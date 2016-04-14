51f2b96b4768c119203d04ad564cf04cf2d2790e
Natalochka1 <natatipaeva@mail.ru>
2016-04-14 22:32:35 +0300

    refresh lesson6

--- slick/js/script.js
+++ slick/js/script.js
@@ -1,1 +1,31 @@
+(function($){
 
+$.fn.myslider = function(option) {
+	var _default = {
+		autoSlide: false,
+		dots: false,
+		arrow:false
+	}
+	console.log(option);
+	console.log(_default);
+	console.log(_setting);
+	var _setting = $.expend(_default, option);
+	var _this = $(this);
+}
+  
+})(jQuery);
+
+
+
+
+//main.js
+
+(function($){
+	$(function() {
+		$('.js-slider').mySlider
+		autoSlide: true;
+		dots: true;
+		arrow:true;
+		newKey: 123344;
+	)};
+	)};
